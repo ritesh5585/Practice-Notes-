0857eda32db79c35f3473bbf13aa6d6b174db5e6
ritesh vishwakarma <vishwakarmaritesh5585@gmail.com>
2026-07-19 21:13:41 +0530

    Working search bar

---
+++ Practice/Search/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const data = ["React", "Node", "MongoDB", "Express"];
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <h1>Search Example</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <h2>Results:</h2>
+      <ul>
+        {data
+          .filter((e) => e.toLowerCase().includes(query.toLowerCase()))
+          .map((items, index) => {
+            return <li key={index}>{itenms}</li>;
+          })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
