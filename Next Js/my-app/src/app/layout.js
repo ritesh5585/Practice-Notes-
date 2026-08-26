28da7f53334bf1fc572f3a92f9c209f3e2c55ac9
Ritesh <vishwakarmaritesh5585@gmail.com>
2026-08-26 23:11:31 +0530

    Fix hydration warning in RootLayout and ensure products are fetched correctly in the product page

--- Next Js/my-app/src/app/layout.js
+++ Next Js/my-app/src/app/layout.js
@@ -10,6 +10,8 @@ export default function RootLayout({ children }) {
     <html
       lang="en"
       className={`h-full antialiased`}
+      suppressHydrationWarning // Add this
+
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
--- Next Js/my-app/src/app/product/page.js
+++ Next Js/my-app/src/app/product/page.js
@@ -2,7 +2,7 @@ import React, { Children } from 'react'
 
 const page = async () => {
   let res = await fetch("https://fakestoreapi.com/products")
-  let products = res.json()
+  let products = await res.json()
 
   console.log(products)
   return (
